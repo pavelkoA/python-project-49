fa0b63e0e29744ec43707bc611bf9b6d77112fc3
pavelkoA <pavelko88@mail.ru>
2023-09-20 11:28:45 +0300

    add welkome_user funk on brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+from brain_games.cli import welkome_user
+
+
 def main():
     print("Welkome to the Brain Games!")
-
+    welkome_user()
 
 if __name__=="__main__":
     main()
