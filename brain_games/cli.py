593250436a0c9fadbcc67f449d1bad1775278ee5
pavelkoA <pavelko88@mail.ru>
2023-09-24 19:37:39 +0300

    mod cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,7 +4,7 @@ import prompt
 def welkome_user():
     print("Welkome to the Brain Games!")
     name = prompt.string("May I have your name? ")
-    print(f"Hello, {name}")
+    print(f"Hello, {name}!")
     return name
 
 
