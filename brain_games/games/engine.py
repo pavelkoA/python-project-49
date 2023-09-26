d93f02358f5431310d6ac617ba2b308f3ccb77fa
pavelkoA <pavelko88@mail.ru>
2023-09-26 18:03:20 +0300

    add engine.py and lexicon.py

---
+++ brain_games/games/engine.py
@@ -0,0 +1,22 @@
+import prompt
+from brain_games.games.lexicon import lexicon
+
+
+def game(name, game_name, game_function):
+    points = 0
+    print(lexicon[game_name])
+    while points != 3:
+        quest, correct_answer = game_function()
+        print(f"Question: {quest}")
+        your_answer = prompt.string("Your answer: ")
+        if correct_answer == your_answer:
+            print('Correct!')
+            points += 1
+        else:
+            print(f"Your answer: '{your_answer}' is wrong answer ;(."
+                  f"Correct answer was '{correct_answer}'")
+            break
+    if points == 3:
+        print(f"Congratulations, {name}!")
+    else:
+        print(f"Let's try again, {name}!")
---
+++ brain_games/games/lexicon.py
@@ -0,0 +1,7 @@
+lexicon = {
+    'progression': 'What number is missing in the progression?',
+    'prime': 'Answer "yes" if given number is prime. Otherwise answer "no".',
+    'even': 'Answer "yes" if the number is even, otherwise answer "no".',
+    'gcd': 'Find the greatest common divisor of given numbers.',
+    'calc': 'What is the result of the expression?'
+}
