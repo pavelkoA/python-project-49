ddee2c21108725c5795139c08b28107aae096755
pavelkoA <pavelko88@mail.ru>
2023-10-08 22:11:12 +0300

    refact calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -5,24 +5,13 @@ from brain_games.engine import play_game
 NAME_GAME_CALC = 'calc'
 
 
-def calculate(num1, num2, oper):
-    correct_answer = 0
-    match oper:
-        case '+':
-            correct_answer = num1 + num2
-        case "-":
-            correct_answer = num1 - num2
-        case "*":
-            correct_answer = num1 * num2
-    return str(correct_answer)
-
-
 def calc_game():
     math_oper = ['+', '-', '*']
     num1, num2 = get_rnd_num(amount=2)
     oper = get_rnd_choice(math_oper)
-    correct_answer = calculate(num1, num2, oper)
-    return f'{str(num1)} {oper} {str(num2)}', correct_answer
+    oper_string = f'{str(num1)} {oper} {str(num2)}'
+    correct_answer = str(eval(oper_string))
+    return oper_string, correct_answer
 
 
 def play_calc():
