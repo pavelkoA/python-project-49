336085de4896a05cb6cc65884817842bebd7af89
pavelkoA <pavelko88@mail.ru>
2023-10-20 15:07:52 +0300

    optim progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,25 +1,25 @@
 
 from brain_games.rnd_utils import get_rnd_num, get_rnd_choice
-from brain_games.engine import launch_games
+from brain_games.engine import start_engine_games
 from brain_games.constant import PROGRES_LENGTH, DESC_PROGRESSION
 
 
-def get_progres_list():
+def get_progression_list():
     start_num = get_rnd_num()
     step = get_rnd_num(end_num=10)
-    progres_list = []
-    while len(progres_list) != PROGRES_LENGTH:
-        progres_list.append(str(start_num))
-        start_num += step
+    progres_list = [str(start_num + step * (number - 1))
+                    for number in range(PROGRES_LENGTH)]
     return progres_list
 
 
+
 def get_task_game_progression():
-    progres_list = get_progres_list()
-    correct_answer = get_rnd_choice(progres_list)
-    progres_list[progres_list.index(correct_answer)] = '..'
-    return " ".join(progres_list), correct_answer
+    progression_list = get_progression_list()
+    rnd_index = get_rnd_num(end_num=PROGRES_LENGTH - 1)
+    correct_answer = progression_list[rnd_index]
+    progression_list[rnd_index] = '..'
+    return " ".join(progression_list), correct_answer
 
 
 def start_game_progression():
-    launch_games(DESC_PROGRESSION, get_task_game_progression)
+    start_engine_games(DESC_PROGRESSION, get_task_game_progression)
